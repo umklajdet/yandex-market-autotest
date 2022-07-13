db70195b4662b834fc9ca91de05ce56d846e4f06
umklajdet <pfedorov85@gmail.com>
2022-07-13 22:01:51 +0300

    обновлен поиск нужного элемента

--- src/main/java/ru/ibs/pages/AudioTechnicsPage.java
+++ src/main/java/ru/ibs/pages/AudioTechnicsPage.java
@@ -2,6 +2,7 @@ package ru.ibs.pages;
 
 import io.qameta.allure.Step;
 import org.junit.Assert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
@@ -9,13 +10,14 @@ import java.util.List;
 
 public class AudioTechnicsPage extends BasePage {
 
-    @FindBy(xpath = "//div[@class='_3Nlow _3Ct1C']")
+    @FindBy(xpath = "//div[@class='_2jsCq _1qPMc']")
     private List<WebElement> categoryList;
 
     @Step("Выбираем категорию {categoryTitle}")
     public HeadsetPage chooseAudioTechnicsElement(String categoryTitle) {
         for (WebElement category : categoryList) {
-            if (category.getText().contains(categoryTitle)) {
+            WebElement listItemChild = category.findElement(By.xpath("//div[@class='_3Nlow _3Ct1C']"));
+            if (listItemChild.getText().contains(categoryTitle)) {
                 waitUntilElementToBeClickable(category);
                 category.click();
                 return pageManager.getHeadsetPage();
